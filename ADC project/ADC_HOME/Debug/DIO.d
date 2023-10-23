37fc8a46afca463fec126c126be3caccafdc91ac
Besho <beshoy.soliman2000@gmail.com>
2023-10-23 23:07:39 +0300

    ADC_project

---
+++ ADC project/ADC_HOME/Debug/DIO.d
@@ -0,0 +1,12 @@
+DIO.o DIO.o: ../DIO.c ../Std_types.h ../Bit_utils.h ../DIO_int.h \
+  ../DIO_prv.h ../DIO_cfg.h
+
+../Std_types.h:
+
+../Bit_utils.h:
+
+../DIO_int.h:
+
+../DIO_prv.h:
+
+../DIO_cfg.h:
---
+++ ADC project/ADC_HOME/Debug/EXTI_prg.d
@@ -0,0 +1,16 @@
+EXTI_prg.o EXTI_prg.o: ../EXTI_prg.c ../Std_types.h ../Bit_utils.h \
+  ../DIO_int.h ../EXTI_int.h ../EXTI_prv.h ../EXTI_cfg.h ../GIE_int.h
+
+../Std_types.h:
+
+../Bit_utils.h:
+
+../DIO_int.h:
+
+../EXTI_int.h:
+
+../EXTI_prv.h:
+
+../EXTI_cfg.h:
+
+../GIE_int.h:
