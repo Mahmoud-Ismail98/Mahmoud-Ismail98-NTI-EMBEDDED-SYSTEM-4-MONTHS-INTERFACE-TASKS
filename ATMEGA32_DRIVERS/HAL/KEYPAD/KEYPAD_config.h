5f847141d81875fc142dbb72a3a2121fe08704eb
Ayman <mohamedayman0103@gmail.com>
2023-10-26 16:34:35 +0300

    KEYPAD_INTEFACING

---
+++ ATMEGA32_DRIVERS/HAL/KEYPAD/KEYPAD_config.h
@@ -0,0 +1,26 @@
+#ifndef KEYPAD_CONFIG_H
+#define KEYPAD_CONFIG_H
+
+
+#define KPD_COLUMN0    DIO_u8CHANNEL24
+#define KPD_COLUMN1    DIO_u8CHANNEL25
+#define KPD_COLUMN2    DIO_u8CHANNEL26
+#define KPD_COLUMN3    DIO_u8CHANNEL27
+
+
+#define KPD_ROW0     DIO_u8CHANNEL28
+#define KPD_ROW1     DIO_u8CHANNEL29
+#define KPD_ROW2     DIO_u8CHANNEL30
+#define KPD_ROW3     DIO_u8CHANNEL31
+
+
+#define KPD_ARR   { {'1','2','3','+'},\
+	                {'4','5','6','-'},\
+					{'7','8','9','/'},\
+                    {'N','0','C','='}    }
+
+
+
+
+
+#endif
